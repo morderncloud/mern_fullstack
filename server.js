104665a3f6b67b232ed522001715bda5298bfded
hollylux <hollyluxl@gmail.com>
2018-05-19 22:33:46 -0400

    Added router

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const mongoose = require('mongoose');
+
+const profile = require('./routes/api/profile');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+
 const app = new express();
 const db = require('./config/keys').dbURI;
 
 mongoose.connect(db).then(() => console.log('DB connected successfully')).catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
 
 port = process.env.PORT || 5000;
 
