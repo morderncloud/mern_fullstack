9df62baa787ce0f91a8bfb0890a169825ff790af
hollylux <hollyluxl@gmail.com>
2018-05-19 21:57:29 -0400

    Connecting to mongo db

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dbURI: 'mongodb://blue:passw0rd@ds129670.mlab.com:29670/devsocial'
+}
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
 const app = new express();
+const db = require('./config/keys').dbURI;
+
+mongoose.connect(db).then(() => console.log('DB connected successfully')).catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
 
